91dd260b0937de16c0b589a809ab2473dfb87662
Fauzan Taqiyuddin <19102103@ittelkom-pwt.ac.id>
2023-04-03 17:04:59 +0700

    UUID integrated

--- app/Models/Categorie.php
+++ app/Models/Categorie.php
@@ -2,11 +2,12 @@
 
 namespace App\Models;
 
+use App\Traits\UUIDAsPrimaryKey;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Categorie extends Model
 {
-    use HasFactory;
+    use HasFactory, UUIDAsPrimaryKey;
     protected $guarded;
 }
